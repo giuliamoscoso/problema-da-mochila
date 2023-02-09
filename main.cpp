82b51b4d130fa36776841d14eb7632cedece570e
Giulia de Oliveira <19deoligi@gmail.com>
2023-02-08 22:03:24 -0300

    Funções de indivíduos e população

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
+#include <cstdlib>
 #include <iostream>
 #include <random>
 using namespace std;
+random_device rd;
 
+    int* Individual(int numItens){
+        int *individuo = (int*)malloc(numItens * sizeof(int));
 
-    int* Indiviual(int numItens){
-        int individuo[numItens];
-
-        random_device rd;
         mt19937 mt(rd());
         uniform_real_distribution<float> dist(0,1);
 
@@ -16,25 +16,31 @@ using namespace std;
         return individuo;
     }
 
-    // int Population(int numIndividuo,int numItens){
-    //     for(int i=0;)
-    //     return 
-    // };
+	int** Population(int numIndividuo,int numItens){
+		int **population = (int**)malloc(numIndividuo * sizeof(int*));
+		
+		for(int i = 0; i < numIndividuo; i++){
+		    population[i] = Individual(numItens);
+		}
+		return population;
+	};
 
 int main(){
-    int pesosValores[10][2]={{4,30},{8,10},{8,30},{25,75},{2,10},{50,100},{6,300},{12,50},{100,400},{8,300}};
-    int pesoMaximo=140;
-    int numCromossomos=100;
-    int geracoes=70;
-    int numItens=(sizeof(pesosValores)/sizeof(*pesosValores));
-    int * resto;
-    // cout<<"itens"<<numItens;
-
-    resto=Indiviual(numItens);
-    for(int i=0;i<10;i++){
-        cout<<resto[i]<<"resto";
-
-    }
+    int pesosValores[10][2] = {{4,30},{8,10},{8,30},{25,75},{2,10},{50,100},{6,300},{12,50},{100,400},{8,300}};
+    int pesoMaximo = 140;
+    int numCromossomos = 100;
+    int geracoes = 70;
+    int numItens = (sizeof(pesosValores)/sizeof(*pesosValores));
+    
+	int **populacao;
+	populacao = Population(10, numItens);
+	
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+		    cout << populacao[i][j] << " ";		
+		}
+		cout<<endl;
+	}
 
     return 0;
 }
